0ef17f9a945d72dd1c48ce720f36f12407968605
akatsukiduck <marotimo016@gmail.com>
2026-06-17 19:11:59 +0100

    second commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,19 @@
-import { Poppins } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import './globals.css';
 
-const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["100","200","300","400","500","600","700","800","900"],
-});
+export const metadata: Metadata = {
+  title: 'Sadam Tech',
+  description: 'Your app description',
+};
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <body className={poppins.className}>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
